0cc4a4b266d30b6014e1682ae8bf2125a81a64e4
evertekitsolutions-del <evertekitsolutions@gmail.com>
2026-09-19 17:50:17 +0530

    feat(ui): export procurement controls

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -40,6 +40,8 @@ export * from "./components/measurements/MeasurementForm";
 export * from "./components/laundry/LaundryList";
 export * from "./components/laundry/LaundryForm";
 export * from "./components/expenses/ExpenseList";
+export * from "./components/purchases/PurchaseList";
+export * from "./components/purchases/ProcurementPanel";
 export * from "./components/suppliers/SupplierList";
 export * from "./components/staff/StaffList";
 export * from "./components/staff/StaffDetail";
