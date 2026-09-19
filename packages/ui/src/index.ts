0c2924644d684008c749b48af378933a37e51fa4
evertekitsolutions-del <evertekitsolutions@gmail.com>
2026-09-19 16:33:33 +0530

    feat(ui): export stock transfer control

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -28,6 +28,7 @@ export * from "./components/forms/Modal";
 export * from "./components/customers/CustomerList";
 export * from "./components/customers/format";
 export * from "./components/products/ProductList";
+export * from "./components/products/StockTransferPanel";
 export * from "./components/sales/PosBilling";
 export * from "./components/sales/NormalBilling";
 export * from "./components/sales/SalesList";
